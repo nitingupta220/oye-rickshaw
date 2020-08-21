4b9b6044d5e1a3dc71ced84d7ed3bafca0925edf
Nitin Gupta <nitingupta@MacBook-Pro.local>
2020-08-21 15:36:06 +0530

    Updates

---
+++ precache-manifest.6e1809b9dc74451010d5bce21aa92457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3335c3556927f6ab7e44ec1897a1e33",
+    "url": "/oye-rickshaw/index.html"
+  },
+  {
+    "revision": "114919f55868f7976d06",
+    "url": "/oye-rickshaw/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "2199b2cd719ebe16bfde",
+    "url": "/oye-rickshaw/static/css/main.50695a3d.chunk.css"
+  },
+  {
+    "revision": "114919f55868f7976d06",
+    "url": "/oye-rickshaw/static/js/2.1f1a0b52.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/oye-rickshaw/static/js/2.1f1a0b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2199b2cd719ebe16bfde",
+    "url": "/oye-rickshaw/static/js/main.f58fa4c9.chunk.js"
+  },
+  {
+    "revision": "9ae43b6f4d938a99b9ed",
+    "url": "/oye-rickshaw/static/js/runtime-main.183ebbeb.js"
+  }
+]);
